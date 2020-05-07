a8bdd6a6f2bca907e208664db3b84e94212ea489
Adam <adamkay123@live.com>
2020-05-06 22:58:26 -0700

    adding friendslist functionality

--- JS/index.js
+++ JS/index.js
@@ -13,22 +13,62 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var docRef = db.collection("users").doc(user.uid);
+        docRef.get().then(function (doc) {
+            if (doc && doc.exists) {
+                const myData = doc.data();
+                const friends = myData.friends;
+                console.log(friends);
+                friends.forEach(friend => {
+                    $("#friendTable").append("<td>", friend, "</td>");
+                });
+            }
+        }).catch(function (error) {
+            console.log("Got an error: ", error);
+        });
+    } else {
+        // No user is signed in.
+    }
+});
+
 function showFriendsOption() {
     document.getElementById("friendForm").style.display = "block";
-  }
-  
-  function closeFriends() {
+    return false;
+}
+
+function closeFriends() {
     document.getElementById("friendForm").style.display = "none";
-  }
-  function showInterestsOption() {
+    return false;
+}
+
+function showInterestsOption() {
     document.getElementById("interestForm").style.display = "block";
-  }
-  
-  function closeInterests() {
+    return false;
+}
+
+function closeInterests() {
     document.getElementById("interestForm").style.display = "none";
-  }
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log("document data:", doc.data());
-    });
-});
+    return false;
+}
+
+function addFriend() {
+    var provEmail = $("input[type=email][name=email]").val();
+    var users = db.collection("users");
+
+    users.where("email", "==", provEmail).get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                let user = firebase.auth().currentUser;
+                db.collection("users").doc(user.uid).update({
+                    friends: firebase.firestore.FieldValue.arrayUnion(doc.data().email)
+                });
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+}
